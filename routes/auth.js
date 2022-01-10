6e967d7e1e5ec80a5003e91c3136d0dd9084a183
Sourabh <kanadesourabh420@gmail.com>
2022-01-10 16:26:16 +0530

    routes and pakages added

--- routes/auth.js
+++ routes/auth.js
@@ -8,6 +8,7 @@ router.post("/register", async (req, res) => {
   const newUser = new User({
     username: req.body.username,
     email: req.body.email,
+    // password: req.body.password,
     password: CryptoJS.AES.encrypt(
       req.body.password,
       process.env.PASS_SEC
@@ -16,7 +17,7 @@ router.post("/register", async (req, res) => {
 
   try {
     const savedUser = await newUser.save();
-    res.status(201).json(savedUser);
+    res.status(200).json(savedUser);
   } catch (err) {
     res.status(500).json(err);
   }
