99e906d9f77266f63d0afb280c01719863c5e41f
Sourabh Kanade <kanadesourabh420@gmail.com>
2022-03-11 19:38:57 +0530

    Update data.js

--- client/src/data.js
+++ client/src/data.js
@@ -55,8 +55,4 @@ export const sliderItems = [
   //     id:4,
   //     img:"https://www.burdastyle.com/pub/media/catalog/product/cache/7bd3727382ce0a860b68816435d76e26/107/BUS-PAT-BURTE-1320516/1170x1470_BS_2016_05_132_front.png",
   //   },
-  //   {
-  //     id:5,
-  //     img:"https://images.ctfassets.net/5gvckmvm9289/3BlDoZxSSjqAvv1jBJP7TH/65f9a95484117730ace42abf64e89572/Noissue-x-Creatsy-Tote-Bag-Mockup-Bundle-_4_-2.png",
-  //   },
   // ]
